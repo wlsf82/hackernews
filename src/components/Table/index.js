66be27eb2aab819ef0fc7061e53f97c787260512
Walmyr <wlsf82@gmail.com>
2018-08-17 00:24:37 +0200

    Make links open in a new tab

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -94,7 +94,7 @@ class Table extends Component {
           {reverseSortedList.map(item =>
             <div key={item.objectID} className='table-row'>
               <span style={{ width: '40%' }}>
-                <a href={item.url}>{item.title}</a>
+                <a href={item.url} target='_blank'>{item.title}</a>
               </span>
               <span style={{ width: '30%' }}>{item.author}</span>
               <span style={{ width: '10%' }}>{item.num_comments}</span>
