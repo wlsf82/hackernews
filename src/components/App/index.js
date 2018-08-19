6258a9bc0df3512244d8d06f5a6c649d2f5994bc
Walmyr <wlsf82@gmail.com>
2018-08-19 14:06:25 +0200

    return undefined instead of empty string in case of error
    This is done to not show the loading component in case of an error
    when trying to retrieve data from the API.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -165,7 +165,7 @@ class App extends Component {
             />
         }
         { error
-          ? ''
+          ? undefined
           : <div className='interactions'>
             <ButtonWithLoading
               isLoading={isLoading}
