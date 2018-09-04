d0e5217e7376da958e2bb3784fc162c265795a02
Walmyr <wlsf82@gmail.com>
2018-09-04 15:58:40 +0200

    Create skeleton for end to end tests

---
+++ __tests__/conf.js
@@ -0,0 +1,23 @@
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+module.exports.config = {
+  baseUrl: 'http://infinite-savannah-93746.herokuapp.com',
+  specs: ['./specs/*.spec.js'],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=1024,768']
+    }
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        displayFailuresSummary: true,
+        displayFailedSpec: true,
+        displaySuiteNumber: true,
+        displaySpecDuration: true
+      })
+    );
+  }
+};
---
+++ __tests__/specs/hackernews.spec.js
@@ -0,0 +1,31 @@
+describe('Hackernews kind off app', () => {
+  beforeEach(() => browser.get('/'));
+
+  it('renders 100 items in the first visit', () => {
+    // Write test logic here.
+  });
+
+  it('quickly shows a loading component when clicking the More button', () => {
+    // Write test logic here.
+  });
+
+  it('renders 200 items after clicking the More button', () => {
+    // Write test logic here.
+  });
+
+  it('quickly shows a loading component when searching for the word "react" for the first time', () => {
+    // Write test logic here.
+  });
+
+  it('renders 100 items after searching for the word "react" for the first time', () => {
+    // Write test logic here.
+  });
+
+  it('does not renders a loading component after searching for "react" and then "redux" again', () => {
+    // Write test logic here.
+  });
+
+  it('shows only 99 items after dismissing one item', () => {
+    // Write test logic here.
+  });
+});
