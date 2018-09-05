6208812232a18d68c44541f7a69041a7a7bb48da
Walmyr <wlsf82@gmail.com>
2018-09-05 13:15:19 +0200

    Fix tests using protractor expected conditions

--- __tests__/specs/hackernews.spec.js
+++ __tests__/specs/hackernews.spec.js
@@ -1,9 +1,13 @@
+const EC = protractor.ExpectedConditions;
+const DEFAULT_TIMEOUT_IN_MS = 5000;
+
 describe('Hackernews kind off app', () => {
   beforeEach(() => browser.get('/'));
 
   it('renders 100 items in the first visit', () => {
     const tableElements = element.all(by.css('.table .table-row'));
 
+    browser.wait(EC.visibilityOf(tableElements.last()), DEFAULT_TIMEOUT_IN_MS);
     expect(tableElements.count()).toBe(100);
   });
 
@@ -11,18 +15,25 @@ describe('Hackernews kind off app', () => {
     const moreButton = element(by.css('.interactions button[type="button"]'));
     const loadingElement = element(by.className('loading'));
 
+    browser.wait(EC.elementToBeClickable(moreButton), DEFAULT_TIMEOUT_IN_MS);
     moreButton.click();
 
-    expect(loadingElement.isDisplayed()).toBe(true);
+    browser.wait(EC.visibilityOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
   });
 
   it('renders 200 items after clicking the More button', () => {
     const moreButton = element(by.css('.interactions button[type="button"]'));
+    const loadingElement = element(by.className('loading'));
 
+    browser.wait(EC.elementToBeClickable(moreButton), DEFAULT_TIMEOUT_IN_MS);
     moreButton.click();
+
+    browser.wait(EC.visibilityOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.stalenessOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
 
     const tableElements = element.all(by.css('.table .table-row'));
 
+    browser.wait(EC.visibilityOf(tableElements.last()), DEFAULT_TIMEOUT_IN_MS);
     expect(tableElements.count()).toBe(200);
   });
 
@@ -31,20 +42,29 @@ describe('Hackernews kind off app', () => {
     const searchButton = element(by.css('.interactions button[type="submit"]'));
     const loadingElement = element(by.className('loading'));
 
+    browser.wait(EC.visibilityOf(searchInputTextField), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.elementToBeClickable(searchButton), DEFAULT_TIMEOUT_IN_MS);
+
     searchInputTextField.clear();
     searchInputTextField.sendKeys('react');
     searchButton.click();
 
-    expect(loadingElement.isDisplayed()).toBe(true);
+    browser.wait(EC.visibilityOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
   });
 
   it('renders 100 items after searching for the word "react" for the first time', () => {
     const searchInputTextField = element(by.css('.interactions input[type="text"]'));
     const searchButton = element(by.css('.interactions button[type="submit"]'));
+    const loadingElement = element(by.className('loading'));
 
+    browser.wait(EC.visibilityOf(searchInputTextField), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.elementToBeClickable(searchButton), DEFAULT_TIMEOUT_IN_MS);
     searchInputTextField.clear();
     searchInputTextField.sendKeys('react');
     searchButton.click();
+
+    browser.wait(EC.visibilityOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.stalenessOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
 
     const tableElements = element.all(by.css('.table .table-row'));
 
@@ -56,19 +76,27 @@ describe('Hackernews kind off app', () => {
     const searchButton = element(by.css('.interactions button[type="submit"]'));
     const loadingElement = element(by.className('loading'));
 
+    browser.wait(EC.visibilityOf(searchInputTextField), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.elementToBeClickable(searchButton), DEFAULT_TIMEOUT_IN_MS);
+
     searchInputTextField.clear();
     searchInputTextField.sendKeys('react');
     searchButton.click();
+
+    browser.wait(EC.visibilityOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
+    browser.wait(EC.stalenessOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
+
     searchInputTextField.clear();
     searchInputTextField.sendKeys('redux');
     searchButton.click();
 
-    expect(loadingElement.isPresent()).not.toBe(true);
+    browser.wait(EC.stalenessOf(loadingElement), DEFAULT_TIMEOUT_IN_MS);
   });
 
   it('shows only 99 items after dismissing one item', () => {
     const dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
 
+    browser.wait(EC.elementToBeClickable(dismissButtonOfFirstItem), DEFAULT_TIMEOUT_IN_MS);
     dismissButtonOfFirstItem.click();
 
     const tableElements = element.all(by.css('.table .table-row'));
