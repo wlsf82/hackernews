166eda77845ff49646430041ae42a136a2e8b9f2
Walmyr <wlsf82@gmail.com>
2018-09-05 14:52:38 +0200

    Start refactoring tests to use the page objects pattern

--- __tests__/specs/hackernews.spec.js
+++ __tests__/specs/hackernews.spec.js
@@ -1,89 +1,61 @@
 const helper = require('protractor-helper');
 
+const Page = require('../page-objects/Sample.po.js');
+
 describe('Hackernews kind off app', () => {
+  const page = new Page();
+
   beforeEach(() => browser.get('/'));
 
   it('renders 100 items in the first visit', () => {
-    const tableElements = element.all(by.css('.table .table-row'));
-
-    helper.waitForElementVisibility(tableElements.last());
-    expect(tableElements.count()).toBe(100);
+    helper.waitForElementVisibility(page.tableElements.last());
+    expect(page.tableElements.count()).toBe(100);
   });
 
   it('quickly shows a loading component when clicking the More button', () => {
-    const moreButton = element(by.css('.interactions button[type="button"]'));
-    const loadingElement = element(by.className('loading'));
+    helper.clickWhenClickable(page.moreButton);
 
-    helper.clickWhenClickable(moreButton);
-
-    helper.waitForElementVisibility(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
   });
 
   it('renders 200 items after clicking the More button', () => {
-    const moreButton = element(by.css('.interactions button[type="button"]'));
-    const loadingElement = element(by.className('loading'));
+    helper.clickWhenClickable(page.moreButton);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
-    helper.clickWhenClickable(moreButton);
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
-
-    const tableElements = element.all(by.css('.table .table-row'));
-
-    helper.waitForElementVisibility(tableElements.last())
-    expect(tableElements.count()).toBe(200);
+    helper.waitForElementVisibility(page.tableElements.last())
+    expect(page.tableElements.count()).toBe(200);
   });
 
   it('quickly shows a loading component when searching for the word "react" for the first time', () => {
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'));
-    const loadingElement = element(by.className('loading'));
+    page.searchForTerm('react');
 
-    helper.clearFieldWhenVisibleAndFillItWithText(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
-
-    helper.waitForElementVisibility(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
   });
 
   it('renders 100 items after searching for the word "react" for the first time', () => {
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'));
-    const loadingElement = element(by.className('loading'));
+    page.searchForTerm('react');
 
-    helper.clearFieldWhenVisibleAndFillItWithText(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
-
-    const tableElements = element.all(by.css('.table .table-row'));
-
-    expect(tableElements.count()).toBe(100);
+    expect(page.tableElements.count()).toBe(100);
   });
 
   it('does not renders a loading component after searching for "react" and then "redux" again', () => {
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'));
-    const loadingElement = element(by.className('loading'));
+    page.searchForTerm('react');
 
-    helper.clearFieldWhenVisibleAndFillItWithText(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
+    page.searchForTerm('redux');
 
-    helper.clearFieldWhenVisibleAndFillItWithText(searchInputTextField, 'redux');
-    helper.clickWhenClickable(searchButton);
-
-    helper.waitForElementNotToBePresent(loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
   });
 
   it('shows only 99 items after dismissing one item', () => {
-    const dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
+    helper.clickWhenClickable(page.dismissButtonOfFirstItem);
 
-    helper.clickWhenClickable(dismissButtonOfFirstItem)
-
-    const tableElements = element.all(by.css('.table .table-row'));
-
-    expect(tableElements.count()).toBe(99);
+    expect(page.tableElements.count()).toBe(99);
   });
 });
