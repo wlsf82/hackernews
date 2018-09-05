aa0fcc34be5c0cc10e08bbcb37d33ed2fdf2c899
Walmyr <wlsf82@gmail.com>
2018-09-05 12:36:11 +0200

    Add minimum logic for the tests to fail

--- __tests__/specs/hackernews.spec.js
+++ __tests__/specs/hackernews.spec.js
@@ -2,30 +2,77 @@ describe('Hackernews kind off app', () => {
   beforeEach(() => browser.get('/'));
 
   it('renders 100 items in the first visit', () => {
-    // Write test logic here.
+    const tableElements = element.all(by.css('.table .table-row'));
+
+    expect(tableElements.count()).toBe(100);
   });
 
   it('quickly shows a loading component when clicking the More button', () => {
-    // Write test logic here.
+    const moreButton = element(by.css('.interactions button[type="button"]'));
+    const loadingElement = element(by.className('loading'));
+
+    moreButton.click();
+
+    expect(loadingElement.isDisplayed()).toBe(true);
   });
 
   it('renders 200 items after clicking the More button', () => {
-    // Write test logic here.
+    const moreButton = element(by.css('.interactions button[type="button"]'));
+
+    moreButton.click();
+
+    const tableElements = element.all(by.css('.table .table-row'));
+
+    expect(tableElements.count()).toBe(200);
   });
 
   it('quickly shows a loading component when searching for the word "react" for the first time', () => {
-    // Write test logic here.
+    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    const searchButton = element(by.css('.interactions button[type="submit"]'));
+    const loadingElement = element(by.className('loading'));
+
+    searchInputTextField.clear();
+    searchInputTextField.sendKeys('react');
+    searchButton.click();
+
+    expect(loadingElement.isDisplayed()).toBe(true);
   });
 
   it('renders 100 items after searching for the word "react" for the first time', () => {
-    // Write test logic here.
+    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    const searchButton = element(by.css('.interactions button[type="submit"]'));
+
+    searchInputTextField.clear();
+    searchInputTextField.sendKeys('react');
+    searchButton.click();
+
+    const tableElements = element.all(by.css('.table .table-row'));
+
+    expect(tableElements.count()).toBe(100);
   });
 
   it('does not renders a loading component after searching for "react" and then "redux" again', () => {
-    // Write test logic here.
+    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    const searchButton = element(by.css('.interactions button[type="submit"]'));
+    const loadingElement = element(by.className('loading'));
+
+    searchInputTextField.clear();
+    searchInputTextField.sendKeys('react');
+    searchButton.click();
+    searchInputTextField.clear();
+    searchInputTextField.sendKeys('redux');
+    searchButton.click();
+
+    expect(loadingElement.isPresent()).not.toBe(true);
   });
 
   it('shows only 99 items after dismissing one item', () => {
-    // Write test logic here.
+    const dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
+
+    dismissButtonOfFirstItem.click();
+
+    const tableElements = element.all(by.css('.table .table-row'));
+
+    expect(tableElements.count()).toBe(99);
   });
 });
