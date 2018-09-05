6a9cf7ddbe5d59e8db9d05050407d097505ec68b
Walmyr <wlsf82@gmail.com>
2018-09-05 16:11:34 +0200

    Better organize tests to remove code duplication

--- __tests__/specs/hackernews.spec.js
+++ __tests__/specs/hackernews.spec.js
@@ -12,50 +12,49 @@ describe('Hackernews kind off app', () => {
     expect(page.table.items.count()).toBe(100);
   });
 
-  it('quickly shows a loading component when clicking the More button', () => {
-    helper.clickWhenClickable(page.interactionsFromBottom.moreButton);
-
-    helper.waitForElementVisibility(page.loading.ellipsis);
-  });
-
-  it('renders 200 items after clicking the More button', () => {
-    helper.clickWhenClickable(page.interactionsFromBottom.moreButton);
-    helper.waitForElementVisibility(page.loading.ellipsis);
-    helper.waitForElementNotToBePresent(page.loading.ellipsis);
-
-    helper.waitForElementVisibility(page.table.items.last());
-    expect(page.table.items.count()).toBe(200);
-  });
-
-  it('quickly shows a loading component when searching for the word "react" for the first time', () => {
-    page.interactionsFromTop.searchForTermAfterClearingTheField('react');
-
-    helper.waitForElementVisibility(page.loading.ellipsis);
-  });
-
-  it('renders 100 items after searching for the word "react" for the first time', () => {
-    page.interactionsFromTop.searchForTermAfterClearingTheField('react');
-
-    helper.waitForElementVisibility(page.loading.ellipsis);
-    helper.waitForElementNotToBePresent(page.loading.ellipsis);
-
-    expect(page.table.items.count()).toBe(100);
-  });
-
-  it('does not renders a loading component after searching for "react" and then "redux" again', () => {
-    page.interactionsFromTop.searchForTermAfterClearingTheField('react');
-
-    helper.waitForElementVisibility(page.loading.ellipsis);
-    helper.waitForElementNotToBePresent(page.loading.ellipsis);
-
-    page.interactionsFromTop.searchForTermAfterClearingTheField('redux');
-
-    helper.waitForElementNotToBePresent(page.loading.ellipsis);
-  });
-
   it('shows only 99 items after dismissing one item', () => {
     helper.clickWhenClickable(page.table.dismissButtonOfFirstItem);
 
     expect(page.table.items.count()).toBe(99);
+  });
+
+  describe('Load more', () => {
+    beforeEach(() => helper.clickWhenClickable(page.interactionsFromBottom.moreButton));
+
+    it('quickly shows a loading component when clicking the More button', () => {
+      helper.waitForElementVisibility(page.loading.ellipsis);
+    });
+
+    it('renders 200 items after clicking the More button', () => {
+      helper.waitForElementVisibility(page.loading.ellipsis);
+      helper.waitForElementNotToBePresent(page.loading.ellipsis);
+
+      helper.waitForElementVisibility(page.table.items.last());
+      expect(page.table.items.count()).toBe(200);
+    });
+  });
+
+  describe('Search', () => {
+    beforeEach(() => page.interactionsFromTop.searchForTermAfterClearingTheField('react'));
+
+    it('quickly shows a loading component when searching for the word "react" for the first time', () => {
+      helper.waitForElementVisibility(page.loading.ellipsis);
+    });
+
+    it('renders 100 items after searching for the word "react" for the first time', () => {
+      helper.waitForElementVisibility(page.loading.ellipsis);
+      helper.waitForElementNotToBePresent(page.loading.ellipsis);
+
+      expect(page.table.items.count()).toBe(100);
+    });
+
+    it('does not renders a loading component after searching for "react" and then "redux" again', () => {
+      helper.waitForElementVisibility(page.loading.ellipsis);
+      helper.waitForElementNotToBePresent(page.loading.ellipsis);
+
+      page.interactionsFromTop.searchForTermAfterClearingTheField('redux');
+
+      helper.waitForElementNotToBePresent(page.loading.ellipsis);
+    });
   });
 });
