c51ea3e213fb45d451d317f5358ec795b57fd918
Walmyr <wlsf82@gmail.com>
2018-09-10 10:05:19 +0200

    Simplify selectors on cypress tests

--- __tests__/cypress/integration/hackernews.spec.js
+++ __tests__/cypress/integration/hackernews.spec.js
@@ -2,17 +2,17 @@ describe("Hackernews fake app", () => {
   beforeEach(() => cy.visit("/"));
 
   it("renders 100 items in the table", () => {
-    cy.get(".table .table-row").should("have.length", 100);
+    cy.get(".table-row").should("have.length", 100);
   });
 
   it("renders 99 items in the table after dismissing the first one", () => {
     cy.get(".table-row .button-inline:first").click();
-    cy.get(".table .table-row").should("have.length", 99);
+    cy.get(".table-row").should("have.length", 99);
   });
 
   it("renders 200 items in the table after clicking 'More'", () => {
     cy.contains("More").click();
-    cy.get(".table .table-row").should("have.length", 200);
+    cy.get(".table-row").should("have.length", 200);
   });
 
   it("renders 100 items after searching for 'react'", () => {
@@ -31,15 +31,15 @@ describe("Hackernews fake app", () => {
 
   it("first became last and last become first when sorting", () => {
     cy.contains("Title").click();
-    cy.get(".table .table-row a:first").then($firstTitleBeforeSorting => {
-      cy.get(".table .table-row a:last").then($lastTitleBeforeSorting => {
+    cy.get(".table-row a:first").then($firstTitleBeforeSorting => {
+      cy.get(".table-row a:last").then($lastTitleBeforeSorting => {
         const firstTitleBeforeSorting = $firstTitleBeforeSorting.text();
         const lastTitleBeforeSorting = $lastTitleBeforeSorting.text();
 
         cy.contains("Title").click();
 
-        cy.get(".table .table-row a:first").then($firstTitleAfterSorting => {
-          cy.get(".table .table-row a:last").then($lastTitleAfterSorting => {
+        cy.get(".table-row a:first").then($firstTitleAfterSorting => {
+          cy.get(".table-row a:last").then($lastTitleAfterSorting => {
             const firstTitleAfterSorting = $firstTitleAfterSorting.text();
             const lastTitleAfterSorting = $lastTitleAfterSorting.text();
 
