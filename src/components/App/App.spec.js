0efb321b8bf0d6d4b506f45514262e5337e776a5
Walmyr <wlsf82@gmail.com>
2021-05-28 01:26:36 +0200

    Add a couple more tests

---
+++ src/components/App/App.spec.js
@@ -0,0 +1,43 @@
+import '../../index.css'
+import * as React from 'react'
+import { mount } from '@cypress/react'
+import App from './'
+
+describe('App', () => {
+  const stories = require('../../../cypress/fixtures/stories')
+
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      {
+        body: {
+          hits: [
+            stories.list[0],
+            stories.list[1]
+          ]
+        }
+      }
+    )
+
+    mount(<App />)
+
+    cy.get('.table-row').should('have.length', stories.list.length)
+  })
+
+  it('dismisses one item', () => {
+    cy.get('button')
+      .contains('Dismiss')
+      .click()
+
+    cy.get('.table-row').should('have.length', stories.list.length - 1)
+  })
+
+  it('loads more items', () => {
+    cy.get('button')
+      .contains('More')
+      .click()
+
+    cy.get('.table-row').should('have.length', stories.list.length * 2)
+  })
+})
