e3191c5b3da7d2966353699c537f1c4f930df8ac
Walmyr <wlsf82@gmail.com>
2021-05-28 17:27:44 +0200

    Add e2e test, and adjust scripts and pipeline

---
+++ cypress/integration/hackernewsStories.spec.js
@@ -0,0 +1,26 @@
+describe('Hackernews Stories App', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**'
+    ).as('getStories')
+
+    cy.visit('/')
+    cy.wait('@getStories')
+
+    cy.get('input[type="text"]')
+      .as('searchField')
+      .should('be.visible')
+      .clear()
+  })
+
+  it('searches by a term and it returns another 100 results', () => {
+    cy.get('@searchField')
+      .type('react{enter}')
+
+    cy.wait('@getStories')
+
+    cy.get('.table-row')
+      .should('have.length', 100)
+  })  
+})
