41df4662187c980029a6700915f09c9f71d4930e
Walmyr <wlsf82@gmail.com>
2023-01-08 23:57:30 +0100

    Upgrde cypress and related libs.
    Also, remove libs not needed anymore.
    
    Adjust Cypress configs, test files, etc.

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (on, config) => {
-  require('cypress-grep/src/plugin')(config)
-
-  if (config.testingType === 'component') {
-    require('@cypress/react/plugins/react-scripts')(on, config)
-  }
-
-  return config
-}
